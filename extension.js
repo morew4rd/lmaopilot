7d69efeb94321f48fc30ee30e278e70235f7ba31
mg <morew4rd@gmail.com>
2024-05-29 22:22:57 -0700

    bootstrap

--- extension.js
+++ extension.js
@@ -1,6 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+let vscode = require('vscode');
+
+let endpoint = 'http://localhost:8080'
+
+async function send_to_api(message) {
+    let payload = JSON.stringify({ prompt: message, n_predict: 512, }); // adjust based on your server's API specification
+
+    try {
+      let response = await fetch(`${endpoint}/completion`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: payload
+      });
+
+      if (!response.ok) throw new Error('Network response was not ok');
+
+      let data = await response.json();
+	  let completions = data.content;
+      return completions; // this should be your chatbot's reply
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+	  throw error;
+    }
+  }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,7 +33,7 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -20,8 +45,30 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('lmaopilot.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from lmaopilot!');
+		// Insert text at the current cursor position
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('LMAO NO FILE');
+				return;
+		} else {
+			let selection = editor.selection;
+			let text = editor.document.getText(selection);
+			text = text || "hello"
+			let output =    send_to_api(text).then((x) => {
+				console.log(x)
+				// vscode.window.showInformationMessage('OUT:' + x);
+				let position = editor.selection.active;
+				editor.edit(edit => {
+					edit.insert(position, "\n----->\n" + x);
+				});
+			})
+
+			// let position = editor.selection.active;
+			// editor.edit(edit => {
+			// 	edit.insert(position, "\n----->\n" + output);
+			// });
+			// vscode.window.showInformationMessage('LMAO FILE');
+		}
 	});
 
 	context.subscriptions.push(disposable);
