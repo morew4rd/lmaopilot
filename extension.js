56040bd33206ae7deb082b3f25af0961658a5753
mg <morew4rd@gmail.com>
2024-06-22 13:48:36 -0700

    WIP

--- extension.js
+++ extension.js
@@ -1,10 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
+// let llama = require('./completions').llama
+
 
 let endpoint = 'http://localhost:8080'
 
-async function send_to_api(message) {
+// async function send_to_api(message, onchunk) {
+// 	const request = llama(message, {n_predict: 800})
+//      for await (const chunk of request) {
+//     //    document.write(chunk.data.content)
+// 		onchunk(chunk.data.content)
+//      }
+// }
+
+async function send_to_api_2(message) {
     let payload = JSON.stringify({ prompt: message, n_predict: 512, }); // adjust based on your server's API specification
 
     try {
@@ -25,7 +35,7 @@ async function send_to_api(message) {
       console.error('There has been a problem with your fetch operation:', error);
 	  throw error;
     }
-  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -42,7 +52,7 @@ async function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lmaopilot.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('lmaopilot.lmao', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Insert text at the current cursor position
@@ -54,7 +64,17 @@ async function activate(context) {
 			let selection = editor.selection;
 			let text = editor.document.getText(selection);
 			text = text || "hello"
-			let output =    send_to_api(text).then((x) => {
+
+			// send_to_api(text, (x) => {
+			// 	console.log(x)
+			// 	// vscode.window.showInformationMessage('OUT:' + x);
+			// 	let position = editor.selection.active;
+			// 	editor.edit(edit => {
+			// 		edit.insert(position, "\n----->\n" + x);
+			// 	});
+			// });
+
+			send_to_api_2(text).then((x) => {
 				console.log(x)
 				// vscode.window.showInformationMessage('OUT:' + x);
 				let position = editor.selection.active;
